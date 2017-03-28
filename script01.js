292b6f70451b2c6fc3b67649020294468e3380a1
Hacking Time <gcgithub@bar.edu>
2017-03-28 16:26:23 -0700

    Updating files...

--- script01.js
+++ script01.js
@@ -1,54 +1,49 @@
 window.onload = initAll;
 var useNums = new Array(76);
 function initAll(){
+
   marquee();
-if (document.getElementById){
-document.getElementById("reload").onclick = anotherCard;
-newCard();
-}
-
-
-
+  if (document.getElementById){
+    document.getElementById("reload").onclick = anotherCard;
+    newCard();
+  }
 }
 function marquee(){
-var rand = Math.floor(Math.random()* 75) + 1;
-document.getElementById("scroll").innerHTML = rand;
+  var rand = Math.floor(Math.random()* 75) + 1;
+  document.getElementById("scroll").innerHTML = rand;
 }
 
 function setSquare(thisSquare) {
-var currSquare = "square" + thisSquare;
-var colPlace = new Array(0,0,0,0,0,1,1,1,1,1,2,2,2,2,3,3,3,3,3,4,4,4,4,4);
-var colBasis = colPlace[thisSquare] * 15;
-var newNum;
+  var currSquare = "square" + thisSquare;
+  var colPlace = new Array(0,0,0,0,0,1,1,1,1,1,2,2,2,2,3,3,3,3,3,4,4,4,4,4);
+  var colBasis = colPlace[thisSquare] * 15;
+  var newNum;
 
-do {
-  newNum = colBasis + getNewNum() + 1;
-} while (useNums[newNum]);
+  do {
+    newNum = colBasis + getNewNum() + 1;
+  }
+  while (useNums[newNum]);
 
 
-if (!useNums[newNum]){
 
 
   useNums[newNum] = true;
   document.getElementById(currSquare).innerHTML = newNum;
   document.getElementById(currSquare).className = "";
   document.getElementById(currSquare).onmousedown = toggleColor;
+
+
+
 }
-
-
-}
-
-
 
 function getNewNum()  {
 
-return(Math.floor(Math.random()* 15));
+  return(Math.floor(Math.random()* 15));
 
 }
 function newCard() {
   for(var i=0;i<24;i++){
-
-setSquare(i);
+    setSquare(i);
 
   }
 }
@@ -72,49 +67,46 @@ var thisSquare = window.event.srcElement;
   }
   if(thisSquare.className == ""){
 
-thisSquare.className = "pickedBg";
+    thisSquare.className = "pickedBg";
   }
   else {
     thisSquare.className = "";
   }
-checkWin();
+  checkWin();
 }
 
 
 function checkWin() {
-var winningOption = -1;
-var setSquares = 0;
-var winners = new Array(31,992,15360,507904,541729,557328,1083458,2162820,4329736,8519745,8659472,16252928);
+  var winningOption = -1;
+  var setSquares = 0;
+  var winners = new Array(31,992,15360,507904,541729,557328,1083458,2162820,4329736,8519745,8659472,16252928);
 
-for (var i=0; i<24; i++) {
-
-
-var currSquare = "square" + i;
-if (document.getElementById(currSquare).className != ""){
+  for (var i=0; i<24; i++) {
 
 
-  document.getElementById(currSquare).className = "pickedBg";
-  setSquares = setSquares | Math.pow(2,i);
-}
+    var currSquare = "square" + i;
+    if (document.getElementById(currSquare).className != ""){
 
-}
-for (var i=0; i<winners.length; i++) {
- if ((winners[i] & setSquares) == winners[i]) {
-winningOption = i;
-
- }
-}
-if (winningOption > -1){
-for (var i=0; i>24; i++){
-if (winners[winningOption] & Math.pow(2,i)) {
-currSquare = "square" + i;
-document.getElementById(currSquare).className = "winningBg";
-
-
-
-}
-
+      document.getElementById(currSquare).className = "pickedBg";
+      setSquares = setSquares | Math.pow(2,i);
+    }
 
   }
-}
+  for (var i=0; i<winners.length; i++) {
+    if ((winners[i] & setSquares) == winners[i]) {
+      winningOption = i;
+
+    }
+  }
+  if (winningOption > -1){
+    for (var i=0; i<24; i++){
+      if (winners[winningOption] & Math.pow(2,i)) {
+        currSquare = "square" + i;
+        document.getElementById(currSquare).className = "winningBg";
+        firethefireworks();
+
+
+      }
+    }
+  }
 }
